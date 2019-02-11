d0ad48bd1cd6cfdaa36ccfc13fd8422802a7ad71
David Mamane <david@datananas.com>
2019-02-11 20:16:33 +0100

    start e2e test with a base user story - go to / (products page) - cart have 0 items - select 2 items of first product in row 1 - go to cart page by clicking on navigation bar button

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,36 @@
 // https://docs.cypress.io/api/introduction/api.html
+import * as g from '../support/getters'
 
 describe('My First Test', () => {
-  it('Visits the app root url', () => {
+  beforeEach(() => {
+    cy.server()
+    cy.route('GET', '/albums', 'fixture:albums.json').as('getAlbums')
+    cy.route('GET', '/photos', 'fixture:photos.json').as('getPhotos')
+  })
+
+  it('I should add two item of first product of the first row and see my cart item with the correct quantity', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.wait(['@getAlbums', '@getPhotos'])
+
+    cy.get(g.cartProductsCount)
+      .should('have.text', '0')
+
+    cy.get(g.productFromGrid(1, 1))
+      .within(() => {
+        cy.get(g.quantityInput)
+          .type('{backspace}2')
+
+        cy.get(g.addToCartButton)
+          .click()
+      })
+
+    cy.get(g.cartProductsCount)
+      .should('have.text', '1')
+
+    cy.get(g.navbarCartLink)
+      .click()
+
+    cy.url()
+      .should('contain', '/cart')
   })
 })
